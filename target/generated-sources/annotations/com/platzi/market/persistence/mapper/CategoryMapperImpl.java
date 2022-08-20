3447b2bcca1b1c2641f7eeb53c6911ca466b2451
Isaias Acosta <89610218+IsaiasAcosta26@users.noreply.github.com>
2022-08-20 14:38:54 -0400

    Add files via upload

---
+++ target/generated-sources/annotations/com/platzi/market/persistence/mapper/CategoryMapperImpl.java
@@ -0,0 +1,49 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Category;
+import com.platzi.market.persistence.entity.Categoria;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-08-17T20:48:30-0400",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 1.8.0_341 (Oracle Corporation)"
+)
+@Component
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public Category toCategory(Categoria categoria) {
+        if ( categoria == null ) {
+            return null;
+        }
+
+        Category category = new Category();
+
+        if ( categoria.getIdCategoria() != null ) {
+            category.setCategoryId( categoria.getIdCategoria() );
+        }
+        category.setCategory( categoria.getDescripcion() );
+        if ( categoria.getEstado() != null ) {
+            category.setActive( categoria.getEstado() );
+        }
+
+        return category;
+    }
+
+    @Override
+    public Categoria toCategoria(Category category) {
+        if ( category == null ) {
+            return null;
+        }
+
+        Categoria categoria = new Categoria();
+
+        categoria.setIdCategoria( category.getCategoryId() );
+        categoria.setDescripcion( category.getCategory() );
+        categoria.setEstado( category.isActive() );
+
+        return categoria;
+    }
+}
